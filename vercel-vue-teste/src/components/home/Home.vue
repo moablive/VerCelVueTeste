92a45ad5717968ae5403791cc4ddd144ed7ac00f
guilherme bonato <guilhermeferrazbonato@gmail.com>
2024-08-12 16:35:43 -0300

    ADD 2

--- vercel-vue-teste/src/components/home/Home.vue
+++ vercel-vue-teste/src/components/home/Home.vue
@@ -1,32 +1,35 @@
 <template>
-    <div>
-      <h1>Teste da API</h1>
-      <ul>
-        <li v-for="atendimento in atendimentos" :key="atendimento.id">
-          {{ atendimento.cliente }} - {{ atendimento.servico }} - {{ atendimento.STATUS }} - {{ atendimento.DATA }}
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        atendimentos: []
-      };
-    },
-    mounted() {
-      axios.get('https://vercel-expres-teste-out4n0ohe-teammoab.vercel.app/atendimentos')
-        .then(response => {
-          this.atendimentos = response.data;
-        })
-        .catch(error => {
-          console.error('Erro ao acessar a API:', error);
-        });
-    }
-  };
-  </script>
-  
+  <div>
+    <h1>Teste da API</h1>
+    <ul>
+      <li v-for="atendimento in atendimentos" :key="atendimento.id">
+        {{ atendimento.cliente }} - {{ atendimento.servico }} - {{ atendimento.STATUS }} - {{ atendimento.DATA }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      atendimentos: []
+    };
+  },
+  mounted() {
+    axios.get('https://vercel-expres-teste-out4n0ohe-teammoab.vercel.app/atendimentos', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        this.atendimentos = response.data;
+      })
+      .catch(error => {
+        console.error('Erro ao acessar a API:', error.response ? error.response.data : error.message);
+      });
+  }
+};
+</script>
